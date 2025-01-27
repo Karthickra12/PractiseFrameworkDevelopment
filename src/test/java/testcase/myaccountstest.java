136808698ab26ae57eebccc53256eaec3bc05314
Karthick <Karthickraja2422@gmail.com>
2025-01-27 13:55:20 +0530

    Second commit

---
+++ src/test/java/testcase/myaccountstest.java
@@ -0,0 +1,17 @@
+package testcase;
+
+import org.testng.annotations.Test;
+
+import testbase.Baseclass;
+
+public class myaccountstest extends Baseclass{
+ 
+	
+	@Test
+	public void myaccounttestverify() {
+		
+	}
+	
+	
+	
+}
